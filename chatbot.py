0199db862692678bccb91fdf874c0bac28f0ce26
rohitdhamija <rohit.dhamija@gmail.com>
2019-05-30 16:13:12 +0530

    first2 commit

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,13 @@
 # Building a chatbot with Deep NLP
+
+# import the libraries
+import numpy as np
+import tensorflow as tf
+import re 
+import time
+
+#### part 1 - data processsing ####
+
+#import the dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
